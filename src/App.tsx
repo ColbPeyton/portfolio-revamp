de64ac4e9a45c4e097f24403fbbdd7dd39e5828b
ColbPeyton <33738479+ColbPeyton@users.noreply.github.com>
2021-05-20 16:12:40 -0500

    create App refs for components, use to scroll to each component. create About, Projects, Footer files and add basic markup. Implement new files in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useRef} from 'react';
+import { WidthProvider } from './contexts/WidthContext';
+import { Header } from './components/Header';
+import { About } from './components/About';
+import { Projects } from './components/Projects';
+import { Footer } from './components/Footer';
 import './App.css';
 
 function App() {
+
+  const headerFef = useRef<HTMLDivElement>(null);
+  const aboutRef = useRef<HTMLDivElement>(null);
+  const projectsRef = useRef<HTMLDivElement>(null);
+  const footerRef = useRef<HTMLDivElement>(null);
+
+  const scrollToComponent = (ref:any):void => {
+    ref.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-app">
+      <WidthProvider>
+        <header className='container-app-header' ref={headerFef}>
+          <Header />
+        </header>
+        <div className='container-app-about' ref={aboutRef}>
+          <About />
+        </div>
+        <div className='container-app-projects' ref={projectsRef}>
+          <Projects />
+        </div>
+        <footer className='container-app-footer' ref={footerRef}>
+          <Footer />
+        </footer>
+      </WidthProvider>
     </div>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Footer = ():JSX.Element => {
+    return(
+        <div className='container-footer'>
+            Footer
+        </div>
+    )
+}
