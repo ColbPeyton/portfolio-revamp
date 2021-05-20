2c8292a62e1f907cb96c0613fda3659cf1a3fe09
ColbPeyton <33738479+ColbPeyton@users.noreply.github.com>
2021-05-20 17:38:03 -0500

    Add scroll to App, Create Scroll file and implelemt context/state. export WidthContext interface

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,12 +3,13 @@ import { WidthProvider } from '../contexts/WidthContext';
 import { Header } from './Header';
 import { About } from './About';
 import { Projects } from './Projects';
+import { Scroll } from './Scroll';
 import { Footer } from './Footer';
 import '../styles/App.scss';
 
 function App() {
 
-  const headerFef = useRef<HTMLDivElement>(null);
+  const headerRef = useRef<HTMLDivElement>(null);
   const aboutRef = useRef<HTMLDivElement>(null);
   const projectsRef = useRef<HTMLDivElement>(null);
   const footerRef = useRef<HTMLDivElement>(null);
@@ -20,9 +21,12 @@ function App() {
   return (
     <div className="container-app">
       <WidthProvider>
-        <header className='container-app-header' ref={headerFef}>
+        <header className='container-app-header' ref={headerRef}>
           <Header />
         </header>
+        <div className='container-app-scroll'>
+          <Scroll scrollToComponent={scrollToComponent} refs={{headerRef, aboutRef, projectsRef, footerRef}} />
+        </div>
         <div className='container-app-about' ref={aboutRef}>
           <About />
         </div>
---
+++ src/components/Scroll.tsx
@@ -0,0 +1,50 @@
+import React,{useContext, useState, useEffect} from 'react';
+import {WidthContext, WidthState} from '../contexts/WidthContext';
+
+interface ScrollProps{
+    refs: {
+        headerRef: React.RefObject<HTMLDivElement>;
+        aboutRef: React.RefObject<HTMLDivElement>;
+        projectsRef: React.RefObject<HTMLDivElement>;
+        footerRef: React.RefObject<HTMLDivElement>;
+    };
+    scrollToComponent: (ref: any) => void;
+}
+
+interface ScrollState{
+    height: number;
+}
+
+export const Scroll = (props:ScrollProps):JSX.Element => {
+
+    const width = useContext<WidthState['width']>(WidthContext);
+    const [height, setHeight] = useState<ScrollState['height']>(window.pageYOffset);
+
+    // check current YOffset of page, render bee icon once threshold is met
+    const updateHeight = () =>{
+        setHeight(window.pageYOffset);
+    }
+
+    useEffect(()=>{
+        window.addEventListener("scroll", updateHeight);
+        return () => {
+          window.removeEventListener("scroll", updateHeight);
+        }
+    })
+  
+    const renderButtonAtYOffset = ():string => {
+        return height >= 150 
+        ? 'active'
+        : 'disabled'
+    }
+
+    const callParentScroll = (ref: React.RefObject<HTMLDivElement>):void => {
+        props.scrollToComponent(ref);
+    }
+
+    return (
+        <div className='container-scroll'>
+            scroll
+        </div>
+    )
+}
