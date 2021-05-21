69bc85199ca770379efad2dae9975619beac17ef
ColbPeyton <33738479+ColbPeyton@users.noreply.github.com>
2021-05-21 00:19:41 -0500

    add context and helper function to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React,{useContext} from 'react';
+import { WidthContext, WidthState } from '../contexts/WidthContext';
+import {renderBasedOnScreen} from '../_utils/renderBasedOnScreenSize';
 
 export const Header = ():JSX.Element => {
+
+    const width = useContext<WidthState['width']>(WidthContext);
+
+    const mobileMarkup = ():JSX.Element => {
+        return(
+            <div className='header-mobile'>
+                mobile
+            </div>
+        )
+    }
+    const desktopMarkup = ():JSX.Element => {
+        return(
+            <div className='header-desktop'>
+                header
+            </div>
+        )
+    }
+
     return(
         <div className='container-header'>
-            Header
+            {renderBasedOnScreen(width, mobileMarkup, desktopMarkup)}
         </div>
     )
 }
