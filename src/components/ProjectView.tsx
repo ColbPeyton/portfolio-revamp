ae05f48137a23d8ff19facdfc8241b303fb34f7b
ColbPeyton <33738479+ColbPeyton@users.noreply.github.com>
2021-05-24 15:31:35 -0500

    add ProjectView to Projects. export ProjectView props. update Projects markup and add state

--- src/components/ProjectView.tsx
+++ src/components/ProjectView.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {Skill} from './Skill';
 
-interface ProjectViewProps{
+export interface ProjectViewProps{
     project:{
         name: string;
         technology: {color:string, title:string}[];
@@ -15,8 +15,8 @@ export const ProjectView = (props: ProjectViewProps):JSX.Element => {
 
     const renderTechnology = ():JSX.Element[] => {
         return props.project.technology.map((
-            tech:{ color: string; title: string}, 
-            index:number):JSX.Element => {
+            tech:{ color: string; title: string}, index:number
+            ):JSX.Element => {
                 return <Skill title={tech.title} color={tech.color} key={index}/>
         })
     }
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React,{useState} from 'react';
+import { ProjectView, ProjectViewProps} from './ProjectView';
+
+interface ProjectState{
+    activeProject: string;
+}
 
 export const Projects = ():JSX.Element => {
+
+    const [activeProject, setActiveProject] = useState<ProjectState['activeProject']>('');
+
+    // const renderList = ():JSX.Element => {
+    //     return(
+
+    //     )
+    // }
+
+    const handleClick = (title: string):void => {
+        if(title === activeProject){
+            setActiveProject('');
+        }else{
+            setActiveProject(title);
+        }
+    }
+
+    const shouldRenderView = (title:string, details:ProjectViewProps):JSX.Element => {
+        return title === activeProject
+        ? <ProjectView project={details.project} />
+        : <div></div>
+    }
+
     return(
         <div className='container-projects'>
-            Projects
+
         </div>
     )
 }
