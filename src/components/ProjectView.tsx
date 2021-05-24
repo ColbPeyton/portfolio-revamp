4cd413af0e2c3584f84dcb1f691f9a86a98ceb80
ColbPeyton <33738479+ColbPeyton@users.noreply.github.com>
2021-05-23 20:24:27 -0500

    add basic markup to ProjectView

--- src/components/ProjectView.tsx
+++ src/components/ProjectView.tsx
@@ -1,9 +1,34 @@
 import React from "react";
 
-export const ProjectView = ():JSX.Element => {
+interface ProjectViewProps{
+    project:{
+        name: string;
+        technology: string[];
+        about: string;
+        url: string;
+        github: string;
+    }
+}
+
+export const ProjectView = (props: ProjectViewProps):JSX.Element => {
+
+    const renderTechnology = ():JSX.Element[] => {
+        return props.project.technology.map((tech:string, index:number):JSX.Element => {
+            return <p key={index}>{tech}</p>
+        })
+    }
+
     return(
         <div className='container-project-view'>
-            
+            <div className='container-view-title'>
+                <h2>{props.project.name}</h2>
+            </div>
+            <div className='container-view-about'>
+                <p>{props.project.about}</p>
+            </div>
+            <div className='container-view-technology'>
+                {renderTechnology()}
+            </div>
         </div>
     )
 }
