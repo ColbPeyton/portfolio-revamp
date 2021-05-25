6bef971962dd4b2ec86ceaa15b04e479f7a5fdfa
ColbPeyton <33738479+ColbPeyton@users.noreply.github.com>
2021-05-25 16:57:08 -0500

    add markup to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
 
+
 export const Footer = ():JSX.Element => {
     return(
-        <div className='container-footer'>
-            Footer
-        </div>
+        <footer className='container-footer'>
+            <div className='footer-top'>
+                <div className='footer-img'>
+                    {/* <img src={profile} alt='me' /> */}
+                </div>
+                <div className='footer-tagline'>
+                    <h3>Let's Work Together.</h3>
+                </div>
+            </div>
+            <div className='footer-bot'>
+                <div className='footer-links'>
+                    <a href='https://github.com/ColbPeyton' target='_blank' rel="noopener noreferrer">GitHub</a>
+                    <a href='https://www.linkedin.com/in/colbypeyton/' target='_blank' rel="noopener noreferrer">LinkedIn</a>
+                    <a href="mailto:cpeyton.dev@gmail.com?subject=Let's Work Together">cpeyton.dev@gmail.com</a> 
+                </div>
+            </div>
+        </footer>
     )
 }
