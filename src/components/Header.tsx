81e866f4c5dab34202337eead1439de9841f1952
ColbPeyton <33738479+ColbPeyton@users.noreply.github.com>
2021-05-26 19:03:27 -0500

    update index styling with scss and reset styling. update Header markup

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import headerLarge from '../assets/background/header_img_large.png';
 import downArrow from '../assets/icons/keyboard_arrow_right-24px.svg';
 
+import '../styles/Header.scss';
+
 interface HeaderProps{
     aboutRef: React.RefObject<HTMLDivElement>;
     scrollToComponent: (ref: any) => void;
@@ -10,13 +12,13 @@ interface HeaderProps{
 export const Header = (props:HeaderProps):JSX.Element => {
 
     return(
-        <header id='header'>
-            <div className='header-container'>
+        <header className='container-header'>
+            <div className='container-header-markup'>
                 <div className='header-image'>
                     <img src={headerLarge} alt='me working on a project'/>
                 </div>
                 <div className='header-content'>
-                    <div>
+                    <div className='header-title'>
                         <h1>Hi.</h1>
                         <h1>I'm Colby.</h1>
                     </div>
@@ -26,7 +28,7 @@ export const Header = (props:HeaderProps):JSX.Element => {
                             Scroll to learn more and see what I'm working on.
                         </p>
                     </div>
-                    <div>
+                    <div className='header-button'>
                         <button onClick={()=> props.scrollToComponent(props.aboutRef)}>
                             <img src={downArrow} alt='down arrow'/>
                         </button>
