08761c176bff6e83aedacfcea105bc7b24d69136
ColbPeyton <33738479+ColbPeyton@users.noreply.github.com>
2021-06-19 16:32:15 -0500

    add conditional rendering for mobile/desktop project markup

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,69 +1,96 @@
-import React,{useState, useContext} from 'react';
-import { ProjectView, ProjectViewProps} from './ProjectView';
-import {data} from '../_data/data';
+import React, { useState, useContext } from 'react';
+import { ProjectView, ProjectViewProps } from './ProjectView';
+import { data } from '../_data/data';
 import '../styles/Projects.scss';
 import gitIcon from '../assets/icons/github.png';
 import earthIcon from '../assets/icons/earth.svg';
-import {WidthState, WidthContext} from '../contexts/WidthContext';
+import { WidthState, WidthContext } from '../contexts/WidthContext';
 
-interface ProjectState{
-    activeProject: string;
+interface ProjectState {
+  activeProject: string;
 }
 
+export const Projects = (): JSX.Element => {
+  const [activeProject, setActiveProject] =
+    useState<ProjectState['activeProject']>('');
+  const width = useContext<WidthState['width']>(WidthContext);
 
-
-export const Projects = ():JSX.Element => {
-
-    const [activeProject, setActiveProject] = useState<ProjectState['activeProject']>('');
-    const width = useContext<WidthState['width']>(WidthContext);
-
-    // const mobileViewList = ():JSX.Element => {
-        
-    // }
-
-    // const desktopViewList = ():JSX.Element => {
-        
-    // }
-
-    const renderList = ():JSX.Element[] => {
-        return data.map((el: ProjectViewProps['project'], index: number) => {
-            return(
-                <div className='container-project-view'>
-                    <div className='mobile-header'>
-                        <a className={'github'} href={el.github}><img src={gitIcon} /></a>
-                        <h3>{el.name}</h3>
-                        <a className={el.url.length > 0 ? 'hosted' : 'not-hosted'} href={el.url}><img src={earthIcon} /></a>
-                    </div>
-                    <button className={`container-project ${el.name.replace(/\./g, '').replace(/ /g, '_')}`} onClick={()=> handleClick(el.name)}>
-                        <h3>{el.name}</h3>
-                    </button>
-                    <div className={`container-view ${el.name === activeProject ? 'active' : 'inactive'}`}>
-                        {el.name === activeProject && <ProjectView project={el} blade={true}/>}
-                    </div>
-                </div>
-            )
-        })
+  const handleClick = (title: string): void => {
+    if (title === activeProject) {
+      setActiveProject('');
+    } else {
+      setActiveProject(title);
     }
+  };
 
-    const handleClick = (title: string):void => {
-        if(title === activeProject){
-            setActiveProject('');
-        }else{
-            setActiveProject(title);
-        }
-    }
-
-
-    return(
-        <div className='container-projects'>
-            <div className='container-title'>
-                <h1>Projects</h1>
+  const mobileViewList = (): JSX.Element => {
+    return (
+      <div className="container-list-mobile">
+        {data.map((el: ProjectViewProps['project'], index: number) => {
+          return (
+            <div className="container-project-view">
+              <div className="mobile-header">
+                <a className={'github'} href={el.github}>
+                  <img src={gitIcon} />
+                </a>
+                <h3>{el.name}</h3>
+                <a
+                  className={el.url.length > 0 ? 'hosted' : 'not-hosted'}
+                  href={el.url}
+                >
+                  <img src={earthIcon} />
+                </a>
+              </div>
+              <div
+                className={`container-project ${el.name
+                  .replace(/\./g, '')
+                  .replace(/ /g, '_')}`}
+              ></div>
             </div>
-            <div className='container-projects-list'>
-                <div className='container-list'>
-                    {renderList()}
-                </div>
+          );
+        })}
+      </div>
+    );
+  };
+
+  const desktopViewList = (): JSX.Element => {
+    return (
+      <div className="container-list-desktop">
+        {data.map((el: ProjectViewProps['project'], index: number) => {
+          return (
+            <div className="container-project-view">
+              <button
+                className={`container-project ${el.name
+                  .replace(/\./g, '')
+                  .replace(/ /g, '_')}`}
+                onClick={() => handleClick(el.name)}
+              >
+                <h3>{el.name}</h3>
+              </button>
+              <div
+                className={`container-view ${
+                  el.name === activeProject ? 'active' : 'inactive'
+                }`}
+              >
+                {el.name === activeProject && (
+                  <ProjectView project={el} blade={true} />
+                )}
+              </div>
             </div>
-        </div>
-    )
-}
+          );
+        })}
+      </div>
+    );
+  };
+
+  return (
+    <div className="container-projects">
+      <div className="container-title">
+        <h1>Projects</h1>
+      </div>
+      <div className="container-projects-list">
+        {width >= 549 ? desktopViewList() : mobileViewList()}
+      </div>
+    </div>
+  );
+};
