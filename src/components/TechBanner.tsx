ec04bc6f9db0755a338cecb08ecc7268eb5e9b36
ColbPeyton <33738479+ColbPeyton@users.noreply.github.com>
2021-07-24 15:03:33 -0500

    add markup and placeholder tech names array. add banner generation function

--- src/components/TechBanner.tsx
+++ src/components/TechBanner.tsx
@@ -1,5 +1,21 @@
 import React from 'react';
 
+const createBannerItem = (tech: string, key: number): JSX.Element => {
+  return (
+    <div className="banner-item" key={key}>
+      <h3>{tech}</h3>
+    </div>
+  );
+};
+
+const techNames = ['Axios', 'Material-Ui', 'Mapbox', 'React Router', 'Redux'];
+
 export const TechBanner = (): JSX.Element => {
-  return <div className="container-techBanner">Tech</div>;
+  return (
+    <div className="container-tech">
+      <div className="container-banner">
+        {techNames.map((tech, i) => createBannerItem(tech, i))}
+      </div>
+    </div>
+  );
 };
