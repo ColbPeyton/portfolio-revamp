94b4bb4fea39b7aaf59982c0c3dc51e46a03c896
ColbPeyton <33738479+ColbPeyton@users.noreply.github.com>
2021-07-28 16:06:30 -0500

    add basic markup to TechPath

--- src/components/TechPath.tsx
+++ src/components/TechPath.tsx
@@ -1,5 +1,24 @@
 import React from 'react';
 
+const generatePathDetails = (title: string, details: string): JSX.Element => {
+  return (
+    <div className="path-item">
+      <h6>{title}</h6>
+      <p>{details}</p>
+    </div>
+  );
+};
+
+const temp = [
+  { title: 'one', details: 'temp1' },
+  { title: 'two', details: 'temp2' },
+  { title: 'three', details: 'temp3' },
+];
+
 export const TechPath = (): JSX.Element => {
-  return <>TechPath</>;
+  return (
+    <div className="container-path">
+      {temp.map((item) => generatePathDetails(item.title, item.details))}
+    </div>
+  );
 };
