3b559765ce4687879234d667187dc04e0aea7194
ColbPeyton <33738479+ColbPeyton@users.noreply.github.com>
2021-07-29 13:50:36 -0500

    udpate markup for path lines. add color var. update generatePath function to include pathlines and color

--- src/components/TechPath.tsx
+++ src/components/TechPath.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
-const generatePathDetails = (title: string, details: string): JSX.Element => {
+const generatePathDetails = (
+  title: string,
+  details: string,
+  color: string
+): JSX.Element => {
   return (
-    <div className="path-item">
-      <h6>{title}</h6>
-      <p>{details}</p>
-    </div>
+    <>
+      <div className="path-item">
+        <h6>{title}</h6>
+        <p>{details}</p>
+      </div>
+      <div className="path-line" style={{ backgroundColor: color }}></div>
+    </>
   );
 };
 
@@ -15,10 +22,14 @@ const temp = [
   { title: 'three', details: 'temp3' },
 ];
 
+const colors = ['#6C60FA', '#0390F8', '#03BFF8', '#FA60EE', '#BF60FA'];
+
 export const TechPath = (): JSX.Element => {
   return (
     <div className="container-path">
-      {temp.map((item) => generatePathDetails(item.title, item.details))}
+      {temp.map((item, i) =>
+        generatePathDetails(item.title, item.details, colors[i])
+      )}
     </div>
   );
 };
