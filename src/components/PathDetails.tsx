6fc847812126d4f069186aea36576e58a9f3713f
ColbPeyton <33738479+ColbPeyton@users.noreply.github.com>
2021-07-30 15:40:55 -0500

    remove generatePathDetails method from TechPath, move to PathDetails component. update markup

--- src/components/PathDetails.tsx
+++ src/components/PathDetails.tsx
@@ -3,16 +3,21 @@ import React from 'react';
 interface PathDetailsProps {
   title: string;
   details: string;
+  color: string;
 }
 
 export const PathDetails = ({
   title,
   details,
+  color,
 }: PathDetailsProps): JSX.Element => {
   return (
     <>
-      {title}
-      {details}
+      <div className="path-item">
+        <h6>{title}</h6>
+        <p>{details}</p>
+      </div>
+      <div className="path-line" style={{ backgroundColor: color }}></div>
     </>
   );
 };
--- src/components/TechPath.tsx
+++ src/components/TechPath.tsx
@@ -1,20 +1,5 @@
 import React from 'react';
-
-const generatePathDetails = (
-  title: string,
-  details: string,
-  color: string
-): JSX.Element => {
-  return (
-    <>
-      <div className="path-item">
-        <h6>{title}</h6>
-        <p>{details}</p>
-      </div>
-      <div className="path-line" style={{ backgroundColor: color }}></div>
-    </>
-  );
-};
+import { PathDetails } from './PathDetails';
 
 const temp = [
   { title: 'one', details: 'temp1' },
@@ -27,9 +12,13 @@ const colors = ['#6C60FA', '#0390F8', '#03BFF8', '#FA60EE', '#BF60FA'];
 export const TechPath = (): JSX.Element => {
   return (
     <div className="container-path">
-      {temp.map((item, i) =>
-        generatePathDetails(item.title, item.details, colors[i])
-      )}
+      {temp.map((item, i) => (
+        <PathDetails
+          title={item.title}
+          details={item.details}
+          color={colors[i]}
+        />
+      ))}
     </div>
   );
 };
